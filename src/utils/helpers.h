a17857b4b6d54128e92c92b16c30683d02183c04
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-08-27 17:07:38 -0700

    resolve conflict

--- src/utils/helpers.h
+++ src/utils/helpers.h
@@ -10,35 +10,20 @@
 
 #define USE_IOT
 
-// undef the C macros to allow the C++ STL to take over
-// This is to have compatibility with various board implementations of the STL
-#undef min
-#undef max
-
 #include <Arduino.h>
 #include <pgmspace.h>
 
-    const static inline std::string toString(int integer)
-    {
-        return String(integer).c_str();
-    }
+	const static inline std::string toString(int integer)
+	{
+		return String(integer).c_str();
+	}
 
 #else
 
-<<<<<<< HEAD
-#include <sstream>
-
-    template <typename T>
-    std::string toString(T val) {
-        std::stringstream stream;
-        stream << val;
-        return stream.str();
-=======
-    static std::string toString(int integer)
-    {
-        return std::to_string(integer);
->>>>>>> parent of 878b983... rewrite json dependency
-    }
+	static std::string toString(int integer)
+	{
+		return std::to_string(integer);
+	}
 
 #endif
 
