6c744c485c9965fd37e801bc510a33cfe28fac88
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2018-09-14 13:15:55 -0700

    add ArduinoJson submodule
    - lightweight
    - suppors all platforms
    
    - rm_submodules.py remove extra files as platformio handles deps on iot, but will still attempt compiling in include/ArduinoJson
    
    - drop json interface

--- test/rm_submodules.py
+++ test/rm_submodules.py
@@ -2,5 +2,8 @@ Import("env")
 import os
 import shutil
 
-if os.listdir('/include/ArduinoJson') :
-shutil.rmtree('/include/ArduinoJson')
+for root, dirs, files in os.walk('../test/include/ArduinoJson'):
+    for f in files:
+        os.unlink(os.path.join(root, f))
+    for d in dirs:
+        shutil.rmtree(os.path.join(root, d))
